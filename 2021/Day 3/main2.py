0c0aa5a0636bcb32a790697caa616032b44a78be
Bryan <brryann21@gmail.com>
2021-12-14 22:38:22 -0500

    3.2 awful terrible solution

--- 2021/Day 3/main2.py
+++ 2021/Day 3/main2.py
@@ -1,5 +1,5 @@
 lines = []
-with open('inputsample.txt') as f:
+with open('input.txt') as f:
     lines = f.readlines()
 
 arrayDir = []
@@ -12,63 +12,73 @@ while i < len(lines[0]):
 
 i = 0
 
-#TODO
-#This needs to be done dynamically as we remove stuff from oxy and co2
-for line in lines:
-    i = 0
-    while i < len(line):
-        if (line[i] != '\n'):
-            if (line[i] == "1"):
-                array[i] += 1
-            elif(line[i] == "0"):
-                array[i] -= 1
-        i += 1
-
 oxy = lines.copy()
 co2 = lines.copy()
-
 oxyNum = ""
 co2Num = ""
 
 i = 0
-print(array)
+temp = oxy.copy()
 while i < len(array):
-    for line in lines:
-        if (len(oxy) == 1):
-            oxyNum = oxy[0]
-            break
-        if line in oxy:
-            if(array[i] >= 0 and line[i] == "0"):
-                if len(oxy) > 1:
-                    oxy.remove(line)
-            elif(array[i] < 0 and line[i] == "1"):
-                if len(oxy) > 1:
-                    oxy.remove(line)
+    oxy.clear()
+    oxy = temp.copy()
+    temp.clear()
+    
+    j = i
+    while j < len(array):
+        array[j] = 0
+        j += 1
+    j = i
+    while j < len(array):
+        for line in oxy:
+            if (line[j] != '\n'):
+                if (line[j] == "1"):
+                    array[j] += 1
+                elif(line[j] == "0"):
+                    array[j] -= 1
+        j += 1
+    for line in oxy:
+        if(array[i] >= 0 and line[i] == "1"):
+            temp.append(line)
+        elif(array[i] < 0 and line[i] == "0"):
+            temp.append(line)
     i += 1
+    if (len(temp) == 1):
+        oxy = temp.copy()
+        break
 
 i = 0
-
+temp.clear()
+temp = co2.copy()
 while i < len(array):
+    co2.clear()
+    co2 = temp.copy()
+    temp.clear()
     
-    for line in lines:
-        if (len(co2) == 1):
-            co2Num = co2[0]
-            break
-        if line in co2:
-            if(array[i] >= 0 and line[i] == "1"):
-                if len(co2) > 1:
-                    co2.remove(line)
-            elif(array[i] < 0 and line[i] == "0"):
-                if len(co2) > 1:
-                    co2.remove(line)
+    j = i
+    while j < len(array):
+        array[j] = 0
+        j += 1
+    j = i
+    while j < len(array):
+        for line in co2:
+            if (line[j] != '\n'):
+                if (line[j] == "1"):
+                    array[j] += 1
+                elif(line[j] == "0"):
+                    array[j] -= 1
+        j += 1
+    for line in co2:
+        if(array[i] >= 0 and line[i] == "0"):
+            temp.append(line)
+        elif(array[i] < 0 and line[i] == "1"):
+            temp.append(line)
     i += 1
+    if (len(temp) == 1):
+        co2 = temp.copy()
+        break
 
-print(oxyNum)
-print(co2Num)
 
-oxyNum = int(oxyNum, 2)
-co2Num = int(co2Num, 2)
-
-print(oxyNum)
-print(co2Num)
-print(oxyNum * co2Num)
+oxyNum = int(oxy[0], 2)
+co2Num = int(co2[0], 2)
+print (oxyNum * co2Num)
---
+++ 2021/Day 3/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+i = 0
+
+# while i < len(array):
+    
+#     for line in lines:
+#         if (len(co2) == 1):
+#             co2Num = co2[0]
+#             break
+#         if line in co2:
+#             if(array[i] >= 0 and line[i] == "1"):
+#                 if len(co2) > 1:
+#                     co2.remove(line)
+#             elif(array[i] < 0 and line[i] == "0"):
+#                 if len(co2) > 1:
+#                     co2.remove(line)
+#     i += 1
